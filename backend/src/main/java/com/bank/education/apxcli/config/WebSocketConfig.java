b22a9fdcd6604b4d75188af4c7666e9adebb3652
dborrellibau <dborrelli@baufest.com>
2026-01-17 22:39:08 -0300

    nombre y icono

--- backend/src/main/java/com/bank/education/apxcli/config/WebSocketConfig.java
+++ backend/src/main/java/com/bank/education/apxcli/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.bank.education.apxcli.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
